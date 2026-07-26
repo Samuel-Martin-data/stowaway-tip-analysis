6416633a1ed294487bacdf0f0fc14bd3bce85354
Samuel Martin <samwhorocks@hotmail.co.uk>
2026-07-26 12:31:55 -0400

    Delete Stowaway scripts directory

--- Stowaway scripts/stowaway_generational_tip_counts.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-SUM(tips_alpha) AS alpha_total_tips,
-SUM(tips_genz) AS genz_total_tips,
-SUM(tips_millennial) AS millennial_tips,
-SUM(tips_genx) AS genx_tips,
-SUM(tips_boomer) AS boomer_tips
-FROM master_sets
